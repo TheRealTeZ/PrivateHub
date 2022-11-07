f927023aa6e867cf56502e6ab892074cb209263a
Enzo <75174432+TheRealTeZ@users.noreply.github.com>
2022-11-07 12:23:19 +0800

    Update Private Loader.lua

--- Private Loader.lua
+++ Private Loader.lua
@@ -1,1 +1,7 @@
-
+if game.PlaceId == 3956818381 then 
+    loadstring(game:HttpGet(("")))()
+elseif game.PlaceId == 9414511685 then 
+    loadstring(game:HttpGet(("")))()
+else
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Private%20Universal%20Hub.lua")))()
+end
