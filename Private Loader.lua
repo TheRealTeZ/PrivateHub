58ebb23fad802d59153780062c91cc0668b64cab
Enzo <75174432+TheRealTeZ@users.noreply.github.com>
2022-11-09 19:29:45 +0800

    Update Private Loader.lua

--- Private Loader.lua
+++ Private Loader.lua
@@ -1,7 +1,36 @@
-if game.PlaceId == 3956818381 then 
-    loadstring(game:HttpGet("", true))()
-elseif game.PlaceId == 9414511685 then 
-    loadstring(game:HttpGet("", true))()
+local CoreGui = game:GetService("StarterGui")
+
+local GameTable = {
+    [6911148748] = "https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Car%20Driving%20Indonesia.lua",
+    [9872472334] = "https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Evade.lua",
+    [9498006165] = "https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Tapping%20Simulator.lua",
+}
+
+
+
+
+if GameTable[game.PlaceId] == nil then
+    CoreGui:SetCore("SendNotification", {
+        Title = "Game not supported.";
+        Text = "Copy discord to clipboard.";
+        Duration = 3;
+    })
+    
+    task.delay(2, function()
+        setclipboard("https://discord.gg/Scwmb8vn")
+            CoreGui:SetCore("SendNotification", {
+            Title = "Loading";
+            Text = "Game was not supported so its now loading Private Universal Hub.";
+            Duration = 3;
+        })
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Private%20Universal%20Hub.lua"), true))()
+    end)
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheRealTeZ/PrivateHub/main/Private%20Universal%20Hub.lua", true))()
+    CoreGui:SetCore("SendNotification", {
+        Title = "Discord";
+        Text = "We would appreciate if you joined our discord (Copied)";
+        Duration = 10;
+    })
+    setclipboard("https://discord.gg/Scwmb8vn")
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
 end
